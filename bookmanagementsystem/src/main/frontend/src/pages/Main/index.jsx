0205df090fa83f63824afa185ef2112267191a0a
change-challenge <dog941128@hotmail.com>
2023-06-28 16:30:44 +0900

    feat: main view 구축 - main view 구축 without searchBar - searchIcon.svg 추가

--- bookmanagementsystem/src/main/frontend/src/pages/Main/index.jsx
+++ bookmanagementsystem/src/main/frontend/src/pages/Main/index.jsx
@@ -1,10 +1,18 @@
-import styled from 'styled-components'
-
-const Hi = styled.h2`
-  color: ${({ theme }) => theme.colors.red1};
-  font-size: ${({ theme }) => theme.fontSize.sz40};
-`
+import * as S from './style'
 
 export default function Main() {
-  return <Hi>Main</Hi>
+  return (
+    <S.Container>
+      <S.InnerWrapper>
+        <S.UpperLine />
+        <S.Title>
+          어서오세요,
+          <br /> 현대오토에버 도서관리시스템입니다.
+        </S.Title>
+        <S.SubGuide>
+          원하는 책이 있다면, 검색창에 도서를 입력해주세요.
+        </S.SubGuide>
+      </S.InnerWrapper>
+    </S.Container>
+  )
 }
