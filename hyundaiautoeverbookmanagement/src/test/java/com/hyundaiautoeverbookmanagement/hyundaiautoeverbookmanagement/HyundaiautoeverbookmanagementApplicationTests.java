af160bfdfe6d5eb11d5e3868e37c011bd5a1f036
change-challenge <59588256+change-challenge@users.noreply.github.com>
2023-07-05 22:56:59 +0900

    hotfix: Spring Start dependency 해결 (#31)
    - MariaDB 연결
    - RestAPI 시작

---
+++ hyundaiautoeverbookmanagement/src/test/java/com/hyundaiautoeverbookmanagement/hyundaiautoeverbookmanagement/HyundaiautoeverbookmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyundaiautoeverbookmanagement.hyundaiautoeverbookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyundaiautoeverbookmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
