4426e826ef313f4bf2fc6fa27e8e729db972c6e5
change-challenge <dog941128@hotmail.com>
2023-07-05 22:55:50 +0900

    hotfix: Spring Start dependency 해결
    - MariaDB 연결
    - RestAPI 시작

---
+++ hyundaiautoeverbookmanagement/src/main/java/com/hyundaiautoeverbookmanagement/hyundaiautoeverbookmanagement/HyundaiautoeverbookmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hyundaiautoeverbookmanagement.hyundaiautoeverbookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyundaiautoeverbookmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyundaiautoeverbookmanagementApplication.class, args);
+	}
+
+}
