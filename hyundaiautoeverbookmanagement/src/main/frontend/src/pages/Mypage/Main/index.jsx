70dcfb287d0aedf387697c64aabf9e4f6c1c132b
change-challenge <dog941128@hotmail.com>
2023-07-17 17:48:34 +0900

    feat: /wishbook /mypage user권한으로 막기(FE) - #53

--- hyundaiautoeverbookmanagement/src/main/frontend/src/pages/Mypage/Main/index.jsx
+++ hyundaiautoeverbookmanagement/src/main/frontend/src/pages/Mypage/Main/index.jsx
@@ -4,53 +4,53 @@ import CircularProgress from '@mui/material/CircularProgress'
 import Box from '@mui/material/Box'
 import { Text } from '../../../components/index'
 import { fetchUserInfo } from '../../../context/UserContext'
+import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import Pagination from '@mui/material/Pagination'
 
 const Mypage = () => {
+  const navigate = useNavigate()
   const [user, setUser] = useState(null)
 
   useEffect(() => {
     const getUserInfo = async () => {
       const userInfo = await fetchUserInfo()
       setUser(userInfo)
+      if (!userInfo) {
+        alert('로그인이 필요한 기능입니다.')
+        navigate('/')
+      }
     }
     getUserInfo()
   }, [])
 
   console.log('user : ', user)
-  if (!user) {
-    return (
-      <Box sx={{ display: 'flex', width: 300, height: 300 }}>
-        <CircularProgress />
-      </Box>
-    )
-  }
 
   return (
-    <S.MypageContainer>
-      <S.InnerWrapper>
-        <S.MypageTitleWrapper>
-          <Text
-            text="내 서재"
-            color={({ theme }) => theme.colors.main}
-            fontWeight={'bold'}
-            fontSize={({ theme }) => theme.fontSize.sz32}
-          />
-        </S.MypageTitleWrapper>
-        <S.MypageGreetingWrapper>
-          <Text
-            text={`${user.name}님, 반갑습니다!`}
-            color={({ theme }) => theme.colors.black}
-            fontWeight={'bold'}
-            fontSize={({ theme }) => theme.fontSize.sz32}
-          />
-        </S.MypageGreetingWrapper>
-        <S.MypageTabsWrapper>
-          <BasicTabs user={user} />
-        </S.MypageTabsWrapper>
-      </S.InnerWrapper>
-    </S.MypageContainer>
+    user && (
+      <S.MypageContainer>
+        <S.InnerWrapper>
+          <S.MypageTitleWrapper>
+            <Text
+              text="내 서재"
+              color={({ theme }) => theme.colors.main}
+              fontWeight={'bold'}
+              fontSize={({ theme }) => theme.fontSize.sz32}
+            />
+          </S.MypageTitleWrapper>
+          <S.MypageGreetingWrapper>
+            <Text
+              text={`${user.name}님, 반갑습니다!`}
+              color={({ theme }) => theme.colors.black}
+              fontWeight={'bold'}
+              fontSize={({ theme }) => theme.fontSize.sz32}
+            />
+          </S.MypageGreetingWrapper>
+          <S.MypageTabsWrapper>
+            <BasicTabs user={user} />
+          </S.MypageTabsWrapper>
+        </S.InnerWrapper>
+      </S.MypageContainer>
+    )
   )
 }
 
